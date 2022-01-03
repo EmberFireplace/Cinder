a03f035b92bb8ca1a1883f855405d2a16bc17e4d
gamma_02 <gammagames06@gmail.com>
2022-01-03 12:54:51 -0800

    did css and server SVG  stuff :)

--- src/components/Message.svelte
+++ src/components/Message.svelte
@@ -6,6 +6,7 @@
     export let testmessage = "first";
     export let testpfp = "https://codehs.com/uploads/5a274a7c54a1f1d318bf4ed019469f6a";
     export let userID = "318895910306185289";
+    export let testDate = "12/13/21 at 5:04:30";
     let userJSON = readMessageFunction(userID);
     $: if(userID != 0) {
         userJSON.then(value => {
@@ -19,14 +20,10 @@
     .message{
         margin-top:5px;
         margin-bottom:20px;
-        color:#d5d3d6;
     }
-    body{
-        background-color: #2c3034;
-    }
+
     .date+time{
         font-size: xx-small;
-        color: #404249;
     }
     .username{
         font-size: large;
@@ -42,22 +39,24 @@
 </style>
 
 <head>
+        <link rel="stylesheet" href="src/cinder-brand-styles.css">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
 <body>
 
-<div class="container message centered" >
+<div class="container message centered dark-mode-text" >
     <div class="row">
         <div class="col-sm-1 user-png">
             <img src="{testpfp}" width="64px" height="64px"
-                 class="rounded-circle" alt="<username>'s profile picture">
+                 class="rounded-circle" alt="{testuser}'s profile picture">
         </div>
         <div class="col-sm-11">
 
             <span class="username"><b>{testuser}</b></span><span
-                class="date+time"><sup>12/13/21 at 5:04:30</sup></span>
+                class="date+time"><sup>{testDate}</sup></span>
             <p>{testmessage}
             </p>
         </div>
--- src/components/Server/CircularServer.svelte
+++ src/components/Server/CircularServer.svelte
@@ -15,23 +15,30 @@
     let serverPossibleURL = getServerIconURLFromID(serverID);
     $: serverPossibleURL.then(value => serverIconURL = value);
 
+
+
 </script>
 
 
-<div class="img-circular" style="background-image: url('{serverIconURL}')" on:click={handleKeyDown}>
-    {#if showServer}
-        <div> hi </div>
-    {/if}
-</div>
+<!--<div class="img-circular" style="background-image: url('{serverIconURL}')" on:click={handleKeyDown}>-->
+<!--    {#if showServer}-->
+<!--        <div> hi </div>-->
+<!--    {/if}-->
+<!--</div> OLD CODE, IGNORE HOPEFULLY-->
+<svg class="img-circular" id="image-fill" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 500 500" xmlns:xlink="http://www.w3.org/1999/xlink" on:click={handleKeyDown}>
+
+    <defs>
+        <pattern id="{serverID}" x="0" y="0" height="100%" width="100%" patternUnits="userSpaceOnUse">
+            <image width="100%" height="100%" xlink:href="{serverIconURL}"></image>
+        </pattern>
+    </defs>
+    <path d="M 250 0 l 250 125 l 0 250 l -250 125 l -250 -125 l 0 -250 Z" style="stroke: rgb(0, 0, 0);" fill="url('#{serverID}')" transform="matrix(1, -0.000004, 0.000004, 1, -0.001, 0.001)"></path>
+
+
+</svg>
 <style>
     .img-circular{
-        width: 200px;
-        height: 200px;
-        background-size: cover;
-        display: block;
-        border-radius: 100px;
-        -webkit-border-radius: 100px;
-        -moz-border-radius: 100px;
-        background-color: #404249;
+
+
     }
 </style>
